fa9bdc84d3f33b77ff4997db02f828000ea322bd
Mr Mutton <mrmutton.in@gmail.com>
2026-04-04 18:58:30 +0530

    corrected indentation

--- backend/server.py
+++ backend/server.py
@@ -586,7 +586,7 @@ logging.basicConfig(
 logger = logging.getLogger(__name__)
 
 
-@app.on_event("shutdown")
+# @app.on_event("shutdown")
 # async def shutdown_db_client():
     # client.close()
-    pass
+    # pass
